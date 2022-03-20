1ffec527d4e68c2566078f25b5cdd38894b6e01a
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-20 13:38:26 -0400

    Update `App.js` File
    - Add conditional variables to show conditional use in React UI display
    - Show/hide comment block through variable boolean shift
    - Nested comment block data in variable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 function App() {
-	const title = 'Blog Post'
-	const body = 'This is my blog post.'
+	const title = 'Blog Post';
+	const body = 'This is my blog post.';
 	const comments = [
-		{id: 1, text: 'Comment One'},
-		{id: 2, text: 'Comment Two'},
-		{id: 3, text: 'Comment Three'},
-	]
-	
+		{ id: 1, text: 'Comment One' },
+		{ id: 2, text: 'Comment Two' },
+		{ id: 3, text: 'Comment Three' },
+	];
+
+	const loading = false;
+	const showComments = true;
+
+	if (loading) return <h1>Loading...</h1>;
+
+	const commentBlock = (
+		<div className='comments'>
+			<h3>Comments ({comments.length})</h3>
+			<ul>
+				{comments.map((comment, index) => (
+					<li key={index}>{comment.text}</li>
+				))}
+			</ul>
+		</div>
+	);
+
 	return (
 		<div className='container'>
 			<h1>{title}</h1>
 			<p>{body}</p>
 			{Math.random() * (5 + 5)}
 
-			<div className="comments">
-				<h3>Comments ({comments.length})</h3>
-				<ul>
-					{comments.map((comment, index) => (
-						<li key={index}>{comment.text}</li>
-					))}
-				</ul>
-			</div>
+			{showComments && commentBlock}
 		</div>
 	);
 }
