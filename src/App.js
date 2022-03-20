eb4a8725e6978ac63a2c5fa2f2d11fbc5d8c91a7
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-20 13:20:05 -0400

    Update `App.js` File
    - Added several variables to see how data can be displayed in React
    - Used JSX to display `title` and `body` dynamically
    - Used JSX to display a random number in an equation
    - Created a section of UI to dynamically display comments on our "blog"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 function App() {
+	const title = 'Blog Post'
+	const body = 'This is my blog post.'
+	const comments = [
+		{id: 1, text: 'Comment One'},
+		{id: 2, text: 'Comment Two'},
+		{id: 3, text: 'Comment Three'},
+	]
+	
 	return (
 		<div className='container'>
-			<h1>My App</h1>
+			<h1>{title}</h1>
+			<p>{body}</p>
+			{Math.random() * (5 + 5)}
+
+			<div className="comments">
+				<h3>Comments ({comments.length})</h3>
+				<ul>
+					{comments.map((comment, index) => (
+						<li key={index}>{comment.text}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
