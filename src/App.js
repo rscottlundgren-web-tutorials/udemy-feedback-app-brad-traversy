47c800219bf16978a70e18a17fe36cf7225e4fcf
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-20 19:31:57 -0400

    Update `App.js`
    - Reverted functional component to prior format
    - Imported and executed `Header` component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
+import Header from './components/Header';
+
 function App() {
-	const title = 'Blog Post';
-	const body = 'This is my blog post.';
-	const comments = [
-		{ id: 1, text: 'Comment One' },
-		{ id: 2, text: 'Comment Two' },
-		{ id: 3, text: 'Comment Three' },
-	];
-
-	const loading = false;
-	const showComments = true;
-
-	if (loading) return <h1>Loading...</h1>;
-
-	const commentBlock = (
-		<div className='comments'>
-			<h3>Comments ({comments.length})</h3>
-			<ul>
-				{comments.map((comment, index) => (
-					<li key={index}>{comment.text}</li>
-				))}
-			</ul>
-		</div>
-	);
-
 	return (
-		<div className='container'>
-			<h1>{title}</h1>
-			<p>{body}</p>
-			{Math.random() * (5 + 5)}
-
-			{showComments && commentBlock}
-		</div>
+		<>
+			<Header />
+			<div className='container'>
+				<h1>My App</h1>
+			</div>
+		</>
 	);
 }
 
