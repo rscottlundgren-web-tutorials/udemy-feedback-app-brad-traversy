e3ee82b53bb41f8d6877aefe6ee8a383dc42442b
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-26 12:54:39 -0400

    Create `data` Directory, `FeedbackData.js` File
    - Created `data` directory in `src` directory
    - Created `FeedbackData.js` to hold fake data for testing purposes
    - Populated `FeedbackData.js` with fake data for testing purposes

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cras semper auctor neque vitae tempus quam pellentesque nec. Purus viverra accumsan in nisl nisi scelerisque eu. Convallis aenean et tortor at risus viverra. Ut pharetra sit amet aliquam id diam maecenas ultricies. Cursus vitae congue mauris rhoncus. Morbi tristique senectus et netus et malesuada fames ac turpis. Gravida cum sociis natoque penatibus. Semper quis lectus nulla at. Posuere ac ut consequat semper viverra nam libero justo laoreet. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate.',
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Velit egestas dui id ornare arcu. Massa placerat duis ultricies lacus sed turpis tincidunt. Tincidunt augue interdum velit euismod in pellentesque massa. Proin libero nunc consequat interdum varius sit amet. In est ante in nibh mauris cursus mattis molestie. Sed cras ornare arcu dui vivamus arcu. Non blandit massa enim nec dui nunc. Nec ullamcorper sit amet risus nullam eget felis eget. Elementum sagittis vitae et leo duis.',
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'At varius vel pharetra vel turpis nunc eget lorem. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. In iaculis nunc sed augue lacus. At tempor commodo ullamcorper a lacus. Ornare arcu dui vivamus arcu felis bibendum ut tristique. At varius vel pharetra vel turpis nunc. Metus vulputate eu scelerisque felis imperdiet proin fermentum. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. Curabitur vitae nunc sed velit dignissim sodales ut. Id semper risus in hendrerit gravida rutrum quisque non. Maecenas pharetra convallis posuere morbi leo urna molestie at elementum. Nisi porta lorem mollis aliquam ut porttitor leo. In hendrerit gravida rutrum quisque non tellus. Bibendum est ultricies integer quis auctor elit sed vulputate. Tortor at auctor urna nunc id.',
+	},
+	{
+		id: 4,
+		rating: 5,
+		text: 'Feugiat nibh sed pulvinar proin. Pulvinar pellentesque habitant morbi tristique senectus et netus et. Eget arcu dictum varius duis at consectetur. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Sagittis eu volutpat odio facilisis mauris. Nunc congue nisi vitae suscipit tellus mauris. Urna nec tincidunt praesent semper feugiat nibh sed. Quis vel eros donec ac odio tempor. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum. Sit amet tellus cras adipiscing enim eu turpis egestas. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Congue quisque egestas diam in arcu cursus euismod quis viverra. Mi bibendum neque egestas congue quisque egestas. Vitae congue eu consequat ac felis donec et. Odio aenean sed adipiscing diam donec adipiscing tristique risus nec. Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Aliquam eleifend mi in nulla posuere. Egestas sed sed risus pretium quam vulputate. Sollicitudin nibh sit amet commodo nulla.',
+	},
+	{
+		id: 5,
+		rating: 9,
+		text: 'Varius quam quisque id diam vel quam elementum. Id semper risus in hendrerit gravida. Turpis tincidunt id aliquet risus feugiat in ante metus dictum. Integer vitae justo eget magna fermentum iaculis eu non. Quisque id diam vel quam elementum pulvinar etiam non quam. Etiam dignissim diam quis enim. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Turpis massa sed elementum tempus egestas sed sed risus pretium. In nulla posuere sollicitudin aliquam. At erat pellentesque adipiscing commodo elit. Congue nisi vitae suscipit tellus mauris a diam maecenas. Non tellus orci ac auctor augue mauris augue neque. Eros donec ac odio tempor orci. Tincidunt praesent semper feugiat nibh. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Hac habitasse platea dictumst vestibulum rhoncus. Arcu bibendum at varius vel pharetra vel turpis. Massa enim nec dui nunc mattis enim ut.',
+	},
+];
+
+export default FeedbackData;
