479e0fe9bd12a93cd7613accf3864b16cbb2fccf
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-26 15:54:01 -0400

    Update Code Formatting

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,14 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
 
 function FeedbackItem({ item, handleDelete }) {
-  return (
+	return (
 		<Card>
 			<div className='num-display'>{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
-        <FaTimes color='purple' />
-      </button>
+			<button onClick={() => handleDelete(item.id)} className='close'>
+				<FaTimes color='purple' />
+			</button>
 			<div className='text-display'>{item.text}</div>
 		</Card>
 	);
