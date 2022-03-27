4e4f3c8ef4ae13699a95adb4ddcb07bf01658af5
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-27 12:34:26 -0400

    Add Additional State, Conditional Parameters
    - Added `btnDisabled` state & setState
    - Added `message` state & setState
    - Added conditional parameters to "Submit" button display
      - Button will display only when >10 characters entered
    - Added `isDisabled` prop to button
    - Added separate "Final Message" display area under text box

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,21 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
 	const [text, setText] = useState('');
+	const [btnDisabled, setBtnDisabled] = useState(true);
+	const [message, setMessage] = useState('');
 
 	const handleTextChange = (e) => {
+    if(text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+			setMessage('Text Must Be At Least 10 Characters.');
+    } else {
+      setBtnDisabled(false);
+			setMessage(null);
+    }
+
 		setText(e.target.value);
 	};
 
@@ -21,8 +34,10 @@ function FeedbackForm() {
 						placeholder='Write A Review...'
 						value={text}
 					/>
-					<Button type='submit'>Send</Button>
+					<Button type='submit' isDisabled={btnDisabled}>Send</Button>
 				</div>
+
+        {message && <div className='message'>{message}</div>}
 			</form>
 		</Card>
 	);
