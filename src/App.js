e5fa1b534ad50741bab017ddb53e419c9da08ce5
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-29 19:41:35 -0400

    Import & Instantiate `FeedbackProvider`
    - Imported `FeedbackProvider`
    - Wrapped return in `FeedbackProvider`

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 import FeedbackData from './data/FeedbackData';
 
@@ -25,29 +26,31 @@ function App() {
 	};
 
 	return (
-		<Router>
-			<Header />
-			<div className='container'>
-				<Routes>
-					<Route
-						exact
-						path='/'
-						element={
-							<>
-								<FeedbackForm handleAdd={addFeedback} />
-								<FeedbackStats feedback={feedback} />
-								<FeedbackList
-									feedback={feedback}
-									handleDelete={deleteFeedback}
-								/>
-							</>
-						}
-					/>
-					<Route path='/about' element={<AboutPage />} />
-				</Routes>
-				<AboutIconLink />
-			</div>
-		</Router>
+		<FeedbackProvider>
+			<Router>
+				<Header />
+				<div className='container'>
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={
+								<>
+									<FeedbackForm handleAdd={addFeedback} />
+									<FeedbackStats feedback={feedback} />
+									<FeedbackList
+										feedback={feedback}
+										handleDelete={deleteFeedback}
+									/>
+								</>
+							}
+						/>
+						<Route path='/about' element={<AboutPage />} />
+					</Routes>
+					<AboutIconLink />
+				</div>
+			</Router>
+		</FeedbackProvider>
 	);
 }
 
