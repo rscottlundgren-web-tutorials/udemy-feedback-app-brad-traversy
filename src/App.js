e3644855695766e6d1ecce70f89d03fe2b8af15f
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-31 18:31:43 -0400

    Move Functions From `App.js` To `FeedbackContext`
    - Remove `uuid` package import
    - Remove `useState` hook import
    - Remove dummy data file import
    - Remove `useState` hook reference
    - Remove `addFeedback` function
    - Remove `deleteFeedback` function
    - Remove `addFeedback` prop from `FeedbackForm` component
    - Remove `deleteFeedback` prop from `FeedbackList` component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
@@ -9,22 +7,7 @@ import AboutIconLink from './components/AboutIconLink';
 import AboutPage from './pages/AboutPage';
 import { FeedbackProvider } from './context/FeedbackContext';
 
-import FeedbackData from './data/FeedbackData';
-
 function App() {
-	const [feedback, setFeedback] = useState(FeedbackData);
-
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
-	};
-
-	const deleteFeedback = (id) => {
-		if (window.confirm('Are you sure you want to delete this feedback?')) {
-			setFeedback(feedback.filter((item) => item.id !== id));
-		}
-	};
-
 	return (
 		<FeedbackProvider>
 			<Router>
@@ -36,9 +19,9 @@ function App() {
 							path='/'
 							element={
 								<>
-									<FeedbackForm handleAdd={addFeedback} />
+									<FeedbackForm />
 									<FeedbackStats />
-									<FeedbackList handleDelete={deleteFeedback} />
+									<FeedbackList />
 								</>
 							}
 						/>
